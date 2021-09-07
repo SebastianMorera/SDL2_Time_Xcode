bd0da24d518fdd9a6b9d237913948c3394565f43
Sebastian Morera <sebas.morera999@hotmail.com>
2021-09-07 16:47:19 -0400

    Initial Commit

---
+++ SDL_Time/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  SDL_Time
+//
+//  Created by Sebastian Morera on 2021-09-07.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
